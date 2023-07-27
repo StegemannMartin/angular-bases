e8ed2571dd75d51dec1559df2b84738cf031b93a
Martin Stegemann <stegemann_97@hotmail.com>
2023-07-27 19:05:44 -0300

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera app de Angular';
+
 }
+  /*
+  public counter: number = 10;
+
+  increaseBy( value: number):void {
+    this.counter += value;
+  }
+  resetCounter(){
+
+    this.counter = 10;
+
+  }*/
+/*
+  increaseBy():void {
+    this.counter += 1;
+  }
+  decreaseBy():void {
+    this.counter -= 1;
+  }
+}
+*/
